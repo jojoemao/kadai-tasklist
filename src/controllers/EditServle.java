dc6c8b634a84e05547ae1ffc2aa0b879a10bc0ea
Kou.Mou <joemao_0407@yahoo.co.jp>
2022-03-28 22:40:14 +0900

    EditServlet.javaを修正

--- src/controllers/EditServle.java
+++ src/controllers/EditServle.java
@@ -33,13 +33,8 @@ public class EditServle extends HttpServlet {
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // TODO Auto-generated method stub
-        response.getWriter().append("Served at: ").append(request.getContextPath());
-    }
 
-    /**
-     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-     */
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
         EntityManager em = DBUtil.createEntityManager();
 
         // 該当のIDのタスク1件のみをデータベースから取得
